fa868c3a35feece03cbdeb94d3f95939d40783d5
Prateek <155889182+prateek539@users.noreply.github.com>
2024-07-31 22:32:17 +0530

    Add files via upload

---
+++ src/electricity/billing/system/Conn.java
@@ -0,0 +1,19 @@
+
+package electricity.billing.system;
+
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "Prateek@123");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
